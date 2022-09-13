7064d76e486fbe7299f78e7c64279a51e51a3740
Manuel Aristarán <jazzido@jazzido.com>
2022-09-13 14:08:44 -0300

    escape output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
 result=$(wget -O - $1 | /pup $2)
+result="${result//'%'/'%25'}"
+result="${result//$'\n'/'%0A'}"
+result="${result//$'\r'/'%0D'}"
+
 echo "::set-output name=result::$result"
